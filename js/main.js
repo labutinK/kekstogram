01fb34f3f38a96a6f31135ceab8d81da1cb377eb
crab-people <benderodrige3@mail.ru>
2022-03-02 00:01:26 +0300

    hashtag and comment functional done

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ import {checkEscapeKey} from './util.js';
 
 generatePictures();
 
+const uploadForm = document.querySelector('#upload-select-image');
 const comment = document.querySelector('.text__description');
 const hashtag = document.querySelector('.text__hashtags');
+const uploadFormSubmit = document.querySelector('.img-upload__submit');
 
 comment.addEventListener('keydown', (evt) => {
   if (checkEscapeKey(evt)){
@@ -35,19 +37,37 @@ comment.addEventListener('input', function() {
   this.reportValidity();
 });
 
-hashtag.addEventListener('blur', function(){
+hashtag.addEventListener('input', function(){
   let hashtagValue = this.value.toLowerCase();
   let arraySplit = hashtagValue.trim().split(' ');
-  arraySplit.forEach((val) => {
-    if(val[0] !== '#'){
-      this.setCustomValidity('Хештеги должны начинаться с "#"');
+  let errorFlag;
+  errorFlag = arraySplit.find((val, ind) => {
+    if (ind > 4){
+      this.setCustomValidity('Можно добавить только 5 хештегов');
+      return true;
     }
-    else if(!(/[!@#$&*%]/.test(val.slice(1, 0)))){
+    else if(val[0] !== '#'){
+      this.setCustomValidity('Хештеги должны начинаться с "#"');
+      return true;
+    }
+    else if(/[!@#$&*'")(></.,~`\]}|[{%]/.test(val.slice(1, val.length))){
       this.setCustomValidity('Хештеги не должны содержать специальных символов');
+      return true;
     }
     else {
       this.setCustomValidity('');
+      return false;
     }
-  })
+  });
+  (!errorFlag) ? this.classList.remove('has-error') : this.classList.add('has-error');
   this.reportValidity();
+});
+
+uploadForm.addEventListener('input', () => {
+  if (comment.classList.contains('has-error') || hashtag.classList.contains('has-error')){
+    uploadFormSubmit.disabled = true;
+  }
+  else {
+    uploadFormSubmit.disabled = false;
+  }
 });
